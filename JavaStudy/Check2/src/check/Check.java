abf5904bf06f2a6ef209179cdd8825eb9f1e87b5
HaruNaito <112502762+HaruNaito@users.noreply.github.com>
2022-09-18 02:57:42 +0900

    1-0
    課題提出

---
+++ JavaStudy/Check2/src/check/Check.java
@@ -0,0 +1,13 @@
+package check;
+
+public class Check {
+	public static void main(String[] args) { }
+    	private String firstName = "内藤";
+    	private String lastName = "遼";
+  
+    	private static String printName(String firstName, String lastName) {
+			 String printName = firstName + lastName;
+			 return printName;
+	 }
+	 }
+

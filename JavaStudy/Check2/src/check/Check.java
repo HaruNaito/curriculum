5322f725f3e981f16f04370c1646ac9c5a69c4ca
HaruNaito <112502762+HaruNaito@users.noreply.github.com>
2022-09-19 22:08:12 +0900

    3-1
    課題提出

--- JavaStudy/Check2/src/check/Check.java
+++ JavaStudy/Check2/src/check/Check.java
@@ -1,13 +1,26 @@
 package check;
 
 public class Check {
-	public static void main(String[] args) { }
-    	private String firstName = "内藤";
-    	private String lastName = "遼";
-  
-    	private static String printName(String firstName, String lastName) {
-			 String printName = firstName + lastName;
-			 return printName;
-	 }
-	 }
+	public static void main(String[]args){}
+	class printName {
+		private String firstName;
+		private  String lastName;
 
+		public printName(String firstName, String lastName) {
+			this.firstName = "内藤";
+			this.lastName = "遼";
+
+			System.out.println("PrintNameのメソッド→" + firstName + lastName);
+
+		}
+		class PetTester {
+			public static void main(String[]args){
+				Pet robot = new Pet("CHECK_CLASS_JAVA", "CHECK_CLASS_HOGE");
+				RobotPet robot2 = new RobotPet("CHECK_CLASS_R2D2", "CHECK_CLASS_LUKE");
+				robot.introduce();
+				robot2.introduce();
+				System.out.println();
+			}
+		}
+	}
+}

7e504874eee73c79263f40ef171e54ca53291bc8
HaruNaito <112502762+HaruNaito@users.noreply.github.com>
2022-09-21 02:10:55 +0900

    3-2
    課題提出

--- JavaStudy/Check2/src/check/Check.java
+++ JavaStudy/Check2/src/check/Check.java
@@ -1,26 +1,45 @@
 package check;
 
 public class Check {
-	public static void main(String[]args){}
-	class printName {
+	public static void chek(String[]args){}
+	class name {
 		private String firstName;
 		private  String lastName;
-
-		public printName(String firstName, String lastName) {
-			this.firstName = "内藤";
-			this.lastName = "遼";
-
-			System.out.println("PrintNameのメソッド→" + firstName + lastName);
-
+		
+		public class Main {
+		    public static void main(String[] args) {
+		    	int  firstName = "内藤";
+		    	int lastName = "遼";
+		    	int ans = printName(firstName, lastName);
+		        System.out.println("PrintNameのメソッド→" + ans );
+		    }
+		    
+		    public static int printName(int firstName, int lastName) {
+		        return firstName + lastName;
+		    }
 		}
 		class PetTester {
 			public static void main(String[]args){
-				Pet robot = new Pet("CHECK_CLASS_JAVA", "CHECK_CLASS_HOGE");
-				RobotPet robot2 = new RobotPet("CHECK_CLASS_R2D2", "CHECK_CLASS_LUKE");
+				Pet robot = new Pet("Constants.CHECK_CLASS_JAVA", "Constants.CHECK_CLASS_HOGE");
+				RobotPet robot2 = new RobotPet("Cconstants.HECK_CLASS_R2D2", "Constants.CHECK_CLASS_LUKE");
 				robot.introduce();
 				robot2.introduce();
 				System.out.println();
 			}
-		}
-	}
-}
+		}}}
+
+
+
+//package test6;
+////public class Local {
+    
+// private int value=0;
+
+//    private int value2=10;
+ 
+
+//    public void Method(){
+
+//        int num = 5;
+
+//        System.out.println(value);

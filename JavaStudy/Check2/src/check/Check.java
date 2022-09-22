edca0d6bb6b7737b70472ecee9b27836b88c1e45
HaruNaito <112502762+HaruNaito@users.noreply.github.com>
2022-09-22 21:39:42 +0900

    Check2.java
    課題提出

--- JavaStudy/Check2/src/check/Check.java
+++ JavaStudy/Check2/src/check/Check.java
@@ -1,45 +1,23 @@
 package check;
 
+import constants.Constants;
+
 public class Check {
-	public static void chek(String[]args){}
-	class name {
-		private String firstName;
-		private  String lastName;
+
+	private static String firstName= "内藤";
+	private static String lastName = "遼";
+
+	private static String printName(String firstName, String lastName) {
+		return firstName + lastName;
+	}
+
+	public static void main(String[] args) {
 		
-		public class Main {
-		    public static void main(String[] args) {
-		    	int  firstName = "内藤";
-		    	int lastName = "遼";
-		    	int ans = printName(firstName, lastName);
-		        System.out.println("PrintNameのメソッド→" + ans );
-		    }
-		    
-		    public static int printName(int firstName, int lastName) {
-		        return firstName + lastName;
-		    }
-		}
-		class PetTester {
-			public static void main(String[]args){
-				Pet robot = new Pet("Constants.CHECK_CLASS_JAVA", "Constants.CHECK_CLASS_HOGE");
-				RobotPet robot2 = new RobotPet("Cconstants.HECK_CLASS_R2D2", "Constants.CHECK_CLASS_LUKE");
-				robot.introduce();
-				robot2.introduce();
-				System.out.println();
-			}
-		}}}
+		System.out.println("PrintNameのメソッド→" + printName(firstName, lastName));
+		Pet robot = new Pet(Constants.CHECK_CLASS_JAVA, Constants.CHECK_CLASS_HOGE);
+		RobotPet robot2 = new RobotPet(Constants.CHECK_CLASS_R2D2, Constants.CHECK_CLASS_LUKE);
+		robot.introduce();
+		robot2.introduce();
+	}
+}
 
-
-
-//package test6;
-////public class Local {
-    
-// private int value=0;
-
-//    private int value2=10;
- 
-
-//    public void Method(){
-
-//        int num = 5;
-
-//        System.out.println(value);

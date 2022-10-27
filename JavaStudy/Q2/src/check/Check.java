9032bf8bbdc2bf23e97870cc0c7987adb10bf74b
HaruNaito <112502762+HaruNaito@users.noreply.github.com>
2022-10-27 23:03:44 +0900

    Q2

---
+++ JavaStudy/Q2/src/check/Check.java
@@ -0,0 +1,23 @@
+package check;
+
+public class Check {
+	public static void main(String[] args) {
+		//Q1 test1インスタンスを生成(苗字と名前は１のものを使うこと)
+		String test1 = "田中太郎";
+    	
+		//Q2 test2インスタンスを生成(苗字と名前は２のものを使うこと)
+		String test2 = "山田花子";
+		
+		//Q3 test1とtest2の2人の名前を表示
+		 System.out.println(test1);
+		 System.out.println(test2);
+		//Q5 上記で作成したtest1インスタンスを利用してCheck2.javaのQ4 ageメソッド動かすこと(※ 先にQ4の問題を解くこと
+
+		//Q6 上記で作成したtest2インスタンスを利用してCheck2.javaのQ4 ageメソッド動かすこと(※ 先にQ4の問題を解くこと)
+		
+		//Q7 Check2.javaのQ4 ageメソッドの出力結果が「値は無効です」となるようにageメソッドを動かすこと(※ 先にQ4の問題を解くこと)
+		 //Q9 上記で作成したtest1インスタンスを利用してCheck2.javaのQ8 nameOneメソッドを動かすこと(※ 先にQ8の問題を解くこと)
+		 
+		 
+	}
+}
